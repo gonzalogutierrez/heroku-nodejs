ecb21cf15f751f14b0daade8c2605b3f2d719068
gonzalogutierrez <gonzaguti85@gmail.com>
2017-08-31 14:06:20 -0300

    cambio en listen

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const db = require("./utils/db.js");
 require('./utils/routes.js')(app);
 
 
-http.listen(8080,function(){
+/*http.listen(8080,function(){
     console.log("Listening on http://127.0.0.1:8080");
-});
+});*/
+
+app.listen(process.env.PORT || 8080);
